5b0541a73e30ccc9957934531b01f4bb7e964a4e
Massimo <massimo.rosas279@gmail.com>
2021-05-14 13:59:26 +0200

    flag size fix + population data fix

--- src/components/Main/CountriesList/Country/index.js
+++ src/components/Main/CountriesList/Country/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-// import flag from '../../../../assets/flag-example.svg';
+import PropTypes from 'prop-types';
 
 import './country.scss';
 
@@ -9,26 +8,40 @@ const Country = ({
   name,
   population,
   region,
-  capital
-}) => (
-  <div className="country">
-    <img className="country-flag" src={flag} alt="#" />
-    <div className="country-data">
-      <h2 className="country-data-name">{name}</h2>
-      <div className="country-data-single">
-        <h3 className="country-data-title">Population: </h3>
-        <span className="country-data-number">{population}</span>
-      </div>
-      <div className="country-data-single">
-        <h3 className="country-data-title">Region: </h3>
-        <span className="country-data-number">{region}</span>
-      </div>
-      <div className="country-data-single">
-        <h3 className="country-data-title">Capital: </h3>
-        <span className="country-data-number">{capital}</span>
+  capital,
+}) => {
+  const handleNumbers = (number) => (
+    number.toString().replace(/(\d)(?=(\d{3})+\b)/g, '$1 ')
+  );
+
+  return (
+    <div className="country">
+      <img className="country-flag" src={flag} alt="#" />
+      <div className="country-data">
+        <h2 className="country-data-name">{name}</h2>
+        <div className="country-data-single">
+          <h3 className="country-data-title">Population: </h3>
+          <span className="country-data-number">{handleNumbers(population)}</span>
+        </div>
+        <div className="country-data-single">
+          <h3 className="country-data-title">Region: </h3>
+          <span className="country-data-number">{region}</span>
+        </div>
+        <div className="country-data-single">
+          <h3 className="country-data-title">Capital: </h3>
+          <span className="country-data-number">{capital}</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+Country.propTypes = {
+  flag: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  population: PropTypes.number.isRequired,
+  region: PropTypes.string.isRequired,
+  capital: PropTypes.string.isRequired,
+};
 
 export default Country;
